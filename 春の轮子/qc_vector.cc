78bf62fcc442096cef7ecb55024e0b2dbdf85eaa
Krewoe <2192629378@qq.com>
2024-07-03 23:30:28 +0800

    vector

--- 春の轮子/qc_vector.cc
+++ 春の轮子/qc_vector.cc
@@ -2,8 +2,8 @@
  * @file qc_vector.cc
  * @author qc
  * @brief 扩充基本的vector类
- * @details 添加扩容机制,缩容机制
- * @version 0.4
+ * @details 添加迭代器
+ * @version 0.5
  * @date 2024-07-02
  *
  * @copyright Copyright (c) 2024
@@ -21,12 +21,22 @@ namespace qc {
 template <class T>
 class vector {
 public:
+    typedef T* iterator;
+public:
     // 杜绝隐式转换,避免用户手滑写错的情况
-    explicit vector(size_t n) {
-        m_data = new T[n]{};
-        memset(m_data, 0, n * sizeof(T));
-        m_size = n;
-        m_capacity = n;
+    // explicit vector(size_t n) {
+    //     m_data = new T[n]{};
+    //     memset(m_data, 0, n * sizeof(T));
+    //     m_size = n;
+    //     m_capacity = n;
+    // }
+
+    explicit vector(size_t n, T val = 0) {
+        // m_data = new T[n]{val}; 只会初始化第一个为val
+        m_data = new T[n];
+        m_size = m_capacity = n;
+        for (size_t i = 0; i < m_size; ++i) 
+            m_data[i] = val;
     }
 
     // 拷贝构造函数, 深拷贝
@@ -60,7 +70,8 @@ public:
 
     // 移动拷贝函数
     vector& operator=(const vector&& v) noexcept {
-        clear();
+        // clear();
+        if (m_data) delete[] m_data;
         m_data = v.m_data;
         m_size = v.m_size;
         m_capacity = v.m_capacity;
@@ -73,13 +84,24 @@ public:
     vector& operator=(vector const& v) {
         // 判断是否自我赋值
         if (v == *this) return v;
-        clear();
+        // clear();
+        reserve(v.m_size);
         m_capacity = m_size = v.m_size;
         if (m_size) {
-            m_data = new T[m_size]{};
+            // m_data = new T[m_size]{};
             memcpy(m_data, v.m_data, sizeof(T) * m_size);
         }
         return *this;
+    }
+
+    // operator = 中无法实现多个参数
+    // assign相当于祛魅版
+    void assgin(iterator first, iterator last) {
+        size_t diff = last - first;
+        for (size_t i = 0; i < diff; ++i) {
+            m_data[i] = *first;
+            ++first;
+        }
     }
 
     ~vector() noexcept { delete[] m_data; }
@@ -135,7 +157,7 @@ public:
     // 缩小容量(请求,并不一定会执行)
     // 不想浪费内存,将大内存释放掉,得到小内存
     void shrink_to_fit() {
-        T *old_data = m_data;
+        T* old_data = m_data;
         m_capacity = m_size;
         // m_size == 0 不代表没有new过
         if (m_size == 0) {
@@ -167,8 +189,12 @@ public:
         }
     }
 
-    void resize(size_t n) {
+    void resize(size_t n, T val = 0) {
         _grow_capacity_until(n);
+        if (n > m_size) {
+            for (size_t i = m_size; i < n; ++i) 
+                m_data[i] = val;
+        }
         m_size = n;
     }
 
@@ -218,12 +244,20 @@ public:
     T const& front() const noexcept { return m_data[0]; }
 
 public:
+    // 迭代器, 注意在Class中 typedef也受访问控制
+    // typedef T* iterator;
+
+    const iterator begin() const { return m_data; }
+
+    const iterator end() const { return m_data + m_size; }
+
+public:
     void push_back(T val) {
         resize(size() + 1);
         back() = val;
         // (*this)[size() - 1] = val;
     }
-
+    // 区间删除(下标版本)
     void erase(size_t index) {
         for (size_t j = index + 1; j < m_size; ++j) {
             // 后面那个东西已经不需要移动给当前位置
@@ -233,13 +267,31 @@ public:
         // size_t right = m_size - index - 1;
         // memcpy(m_data + index, m_data + index + 1, sizeof(T) * right);
     }
-    // 区间删除
+
     void erase(size_t ibeg, size_t iend) {
         size_t diff = iend - ibeg;
         for (size_t j = iend; j < m_size; ++j) {
             m_data[j - diff] = std::move(m_data[j]);
         }
         resize(m_size - diff);
+    }
+
+    // 区间删除(迭代器版本)
+    void erase(iterator const it) {
+        for (iterator i = it + 1; i < end(); ++i) *(i - 1) = std::move(*i);
+        resize(size() - 1);
+    }
+
+    void erase(iterator const first, iterator const last) {
+        size_t diff = last - first;
+        for (iterator it = last; it != end(); ++it) {
+            *(it - diff) = std::move(*it);
+        }
+        resize(size() - diff);
+    }
+
+    void insert(iterator const it) {
+
     }
 
 private:
@@ -284,13 +336,38 @@ int main() {
     std::cout << "==================================" << std::endl;
 
     qc::vector<int> a;
-    a.reserve(16);
-    for (size_t i = 0; i < 16; ++i) a.push_back(i);
+    a.reserve(4);
+    for (size_t i = 0; i < 4; ++i) a.push_back(i);
 
     qc::vector<int> shrink;
     shrink.reserve(1000);
     shrink.shrink_to_fit();
-    std::cout << "shrink.size() = " << shrink.size() << " shrink.capacity() = " << shrink.capacity() << std::endl;
+    std::cout << "shrink.size() = " << shrink.size()
+              << " shrink.capacity() = " << shrink.capacity() << std::endl;
+
+    for (qc::vector<int>::iterator it = a.begin(); it != a.end(); ++it)
+        printf("a = %d\n", *it);
+
+    std::cout << "======== test_iterator ========" << std::endl;
+    qc::vector<int> b;
+    b.resize(10);
+    int value = 1;
+    for (qc::vector<int>::iterator it = b.begin(); it != b.end(); ++it) {
+        *it = value++;
+        std::cout << *it << std::endl;
+    }
+    qc::vector<int>::iterator it = b.begin() + 1;
+    // 删除第二个元素
+    b.erase(it);
+    for (qc::vector<int>::iterator it = b.begin(); it != b.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+
+    auto first = b.begin() + 1, last = b.begin() + 3;
+    b.erase(first, last);
+    for (qc::vector<int>::iterator it = b.begin(); it != b.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
 
     return 0;
 }
