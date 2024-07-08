2dce7841f331069bdb0316cd8bacff345ba3b311
Krewoe <2192629378@qq.com>
2024-07-08 21:03:28 +0800

    simple_test/

---
+++ simple_test/test_type_const.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+template <class T>
+void func(T const &val) {
+    std::cout << "val = " << val << std::endl;
+}
+
+int main() {
+
+    func<int>(100);
+
+    return 0;
+}
+
