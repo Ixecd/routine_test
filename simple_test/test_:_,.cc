5f93af09324131dda69e27a8cf96779da9701c88
Krewoe <2192629378@qq.com>
2024-07-10 22:28:47 +0800

    simple_test/

--- simple_test/test_:_,.cc
+++ simple_test/test_:_,.cc
@@ -3,7 +3,6 @@ using namespace std;
 
 int main() {
     int op = 1, mask = 0, arg1 = 7, arg2 = 10;
-
     // 防御性编程->该带的括号就得带上
     op = mask != 0 ? (op + arg1, op + arg2) : (op + arg2, op + arg1);
 
