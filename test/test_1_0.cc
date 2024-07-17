fcc7ad1af6a79dee3a839fb3e84569d0a9d1d9c8
Krewoe <2192629378@qq.com>
2024-07-17 18:01:47 +0800

    test

---
+++ test/test_1_0.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#include "qc.hpp"
+using namespace std;
+
+constexpr size_t n = 1 << 26;
+
+int main() {
+    vector<int> vec(n);
+
+    TICK(write_1);
+    for (size_t i = 0; i < vec.size(); i++) vec[i] = 1;
+    TOCK(write_1);
+
+    TICK(write_0);
+    for (size_t i = 0; i < vec.size(); i++) vec[i] = 0;
+    TOCK(write_0);
+
+    return 0;
+}
