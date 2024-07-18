2da92d9cbf5547d9517ef2501ffd05b317ab7f94
Krewoe <2192629378@qq.com>
2024-07-18 22:52:44 +0800

    test

---
+++ test/test_bytes_order.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int abs(int x) {
+    int mask = x >> 31;  // 如果是负数,符号位会顺下来 1111 1111 -> -1
+    printf("mask = %d\n", mask);
+    return (mask + x) ^ mask;
+}
+
+int main() {
+    // 1,2,3,4  低位----->高位
+    // 内存 0x01020304  04 -> 低位, 01 -> 高位, 靠近x的时高位
+    // 内存访问是从高地址向低地址
+    char buf[4] = {1, 2, 3, 4};
+    printf("0x%08x\n", *(int*)buf);  // 小端 -> 4
+
+    int n = 1;
+    n = 100;
+    int b[n] = {1, 2, 3, 4};
+    n = 200;
+    int c[n];
+
+    // std::cout << sizeof(b) << std::endl;
+    // std::cout << sizeof(c) << std::endl;
+
+    int a = -128;
+    printf("%d", abs(a));
+
+    return 0;
+}
---
+++ test/test_size_t.cc
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    std::cout << "sizeof(size_t) = " << sizeof(size_t) << std::endl;
+    std::cout << "sizeof(uintptr_t) = " << sizeof(uintptr_t) << std::endl;
+    return 0;
+}
