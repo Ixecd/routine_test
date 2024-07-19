0464cc600247285e9899a38f71803ece788ba81d
Krewoe <2192629378@qq.com>
2024-07-19 22:29:44 +0800

    test

---
+++ test/samenameforvalfun.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool has_value = false;
+bool has_value() { return has_value; }
+
+int main() {
+    cout << has_value() << endl;
+
+    return 0;
+}
