a9775e87ec0e26b123ade0ebfe46206f95fd5dbd
Krewoe <2192629378@qq.com>
2024-07-20 13:14:03 +0800

    optional

--- 春の轮子/qc_optional.cc
+++ 春の轮子/qc_optional.cc
@@ -2,13 +2,19 @@
 #include <memory>
 #include <type_traits>
 #include <utility>
+#include <vector>
 namespace qc {
 
 // tag类,表示空
 struct nullopt_t {
     explicit nullopt_t() = default;
 };
-inline nullopt_t nullopt;
+inline constexpr nullopt_t nullopt;
+
+struct in_place_t {
+    explicit in_place_t() = default;
+};
+inline constexpr in_place_t in_place;
 
 class bad_optional_access : public std::exception {
 public:
@@ -29,7 +35,12 @@ public:
     };
 
 public:
-    optional(T value) : _value(std::move(value)), _has_value(true) {}
+    // C++20 explicit(bool(exp))
+    explicit(false) optional(T const& value)
+        : _value(std::move(value)), _has_value(true) {}
+
+    explicit(false) optional(T&& value)
+        : _value(std::move(value)), _has_value(true) {}
 
     optional() : _has_value(false) {}
     /// @brief 用户使用nullopt调用下面这个函数,nullopt的类型就是nullopt_t
@@ -46,6 +57,16 @@ public:
         if (_has_value)
             std::construct_at(std::addressof(_value), std::move(that._value));
     }
+
+    template <class... Ts>
+    explicit optional(in_place_t, Ts&&... value_args)
+        : _has_value(true), _value(std::forward<Ts>(value_args)...) {}
+
+    // optional<vector<int>> ov({1,2,3,4})
+    template <class U, class... Ts>
+    explicit optional(in_place_t, std::initializer_list<U> ilist,
+                      Ts&&... value_args)
+        : _has_value(true), _value(ilist, std::forward<Ts>(value_args)...) {}
 
     optional& operator=(optional const& that) {
         if (std::addressof(that) == this) return *this;
@@ -89,7 +110,7 @@ public:
         _has_value = true;
         return *this;
     }
-
+    // T&& 传进来会退化成指针
     optional& operator=(T&& val) noexcept {
         if (_has_value) {
             std::destroy_at(std::addressof(_value));
@@ -291,6 +312,51 @@ public:
         else
             return nullopt;
     }
+
+    // 相比于value_or or_else性能上更高一点,延迟调用
+    template <class F>
+    optional or_else(F&& f) {
+        if (!_has_value)
+            return std::forward<F>(f());
+        else
+            return *this;
+    }
+    // using std::swap 
+    // C++ ADL机制argument dependent lookup
+    // 以此可以实现多态,在namespace中加成员函数,可以实现多态
+    // 如果在自己的namespace中有swap,再自己的namespace中的class有swap就会调用自己namespace中的swap
+    // namespace MySpace {
+    //     class MyClass() {};
+    //     void doSomething(MyClass) {}
+    // }
+    // MySpace::MyClass obj;
+
+    // int func() {
+    //     // MySpace::doSomething(obj);
+    //     doSomething(obj); //right, 自动从参数的名字空间中找,并不一定是全局的
+    // }
+
+    void swap(optional &that) noexcept {
+        if (_has_value && that._has_value) {
+            using std::swap; // ADL
+            // 如果这里的_value 的类型为C,C在namespace MyC中,根据ADL就会去找MyC中是否有swap有的话就用MyC中的,没有的话就用std::swap
+            swap(_value, that._value); // 判断自己有没有实现,有的话用自己的,没有的话用std里的
+        } else if (!_has_value && !that._has_value) return;
+        else if (_has_value) {
+            that.emplace(std::move(_value));
+            reset();
+        } else {
+            emplace(std::move(that.m_value));
+            that.reset();
+        }
+    }
+
+void swap(optional<int> &lhs, optional<int> &rhs) {
+    printf("qc::swap()...");
+    std::swap(lhs, rhs);
+}
+
+
 };
 
 #if __cpp_deduction_guides
@@ -301,6 +367,8 @@ optional(T) -> optional<T>;
 }  // namespace qc
 
 #include <iostream>
+namespace MyC {
+
 struct C {
     C(int x, int y) : m_x(x), m_y(y) {
         std::cout << "C(int, int)" << std::endl;
@@ -316,12 +384,28 @@ struct C {
 
     int operator&() const { return m_x; }
 
+    C & operator=(C && that) {
+        m_x = that.m_x;
+        m_y = that.m_y;
+        std::destroy_at(&that.m_x);
+        std::destroy_at(&that.m_y);
+    }
+
     int value() const { return m_x; }
 
     int m_x;
     int m_y;
 };
 
+void swap(C &lhs, C &rhs) {
+    std::cout << "MyC::swap()" << std::endl;
+    // 要自定义移动赋值和移动拷贝
+    std::swap(lhs, rhs);
+}
+
+}
+
+using namespace MyC;
 void test_union() {
     union U {
         ~U() {}
@@ -380,7 +464,7 @@ qc::optional<int> parseInt(std::string s) {
 }
 
 void test_emplace() {
-    // emplace少了一次移动赋值
+    // emplace少了一次移动拷贝
     // C (int, int)
     // C (C&&)
     //  ~C()
@@ -427,6 +511,22 @@ void test_as_bool() {
     std::cout << "i = " << i << std::endl;  // int i = (int)((bool)opt)
 }
 
+void test_inplace_vector() {
+    qc::optional<std::vector<int>> ov(qc::in_place, {1, 2, 3});
+    std::cout << ov.has_value() << std::endl;
+}
+
+// std::vector<string> v;
+// void feed(std::string &&s) {
+//     throw qc::bad_optional_access();
+//     v.push_back(std::move(s));
+// }
+// std::string s;
+// feed(std::move(s));
+
+/// 注意上面的feed的参数类型,如果是&&传递的是引用,调用feed会类型退化成指针,就算throw也不会影响外面的s
+/// 如果是值传递,使用std::move(s)不会退化,直接将所有权转交给feed的形参,throw之后外面的就用不了
+
 int main() {
     // qc::optional<C> opt(C(1, 2));
     // 这里optional<C>
@@ -441,11 +541,13 @@ int main() {
 
     // test_operator();
 
-    test_emplace();
+    // test_emplace();
 
     // test_pointer();
 
     // test_as_bool();
+
+    test_inplace_vector();
 
     return 0;
 }
