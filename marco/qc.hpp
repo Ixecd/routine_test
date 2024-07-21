f0e40e886f94ebda16d4717f84332005137e30b3
Krewoe <2192629378@qq.com>
2024-07-21 22:33:16 +0800

    qc_marco

--- marco/qc.hpp
+++ marco/qc.hpp
@@ -1,5 +1,10 @@
 #pragma once
 
 #include <chrono>
+#include <iostream>
+
 #define TICK(x) auto bench_##x = std::chrono::steady_clock::now();
 #define TOCK(x) std::cout << #x " : " << std::chrono::duration_cast<std::chrono::duration<double, std::milli>> (std::chrono::steady_clock::now() - bench_##x).count() << " ms" << std::endl;
+
+#define PRINT(x) std::cout << #x " : " << x << std::endl;
+#define PRINT_S(x) std::cout << #x << std::endl;
